9e42e5ef47428d412e70f79457cbf408d940deb8
William Bradley <willmbradley2007@gmail.com>
2026-09-11 02:14:36 +0000

    pluh

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 		System.out.println("Random words");
+
+		int x = 0;
+		if(x == 0){
+			System.out.println("we are in");
+		}
 	}
 }
